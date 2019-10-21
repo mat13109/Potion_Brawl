438e0a54eb3a45828c982f9cf061900d22af7dd8
mat13109 <letesteurdelepique@gmail.com>
2019-10-21 14:51:46 +0200

    Bomb
    Here is the bomb prefab

---
+++ Project Satan/Assets/Scripts/Bomb/BombBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombBehavior : MonoBehaviour
+{
+    float explosionRadius = 5;
+    float radius = 10;
+    float power = 500;
+
+    private void Start()
+    {
+        Explode();
+    }
+
+    void Explode()
+    {
+        Vector3 explosionPosition = transform.position;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(explosionPosition, radius);
+        foreach (Collider2D hit in colliders)
+        {
+            Rigidbody2D rb = hit.GetComponent<Rigidbody2D>();
+            if (rb != null)
+                rb.AddExplosionForce(power, explosionPosition, radius, 1, ForceMode2D.Impulse);
+        }
+    }
+
+    
+}
