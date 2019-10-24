3ce838a39339531a441b7bb7de5af02a5ce2977d
Ajirogg <raimundo.guillaume@gmail.com>
2019-10-24 16:55:51 +0200

    Add glowUp on Bombs

--- Project Satan/Assets/Scripts/Bomb/BombBehavior.cs
+++ Project Satan/Assets/Scripts/Bomb/BombBehavior.cs
@@ -29,9 +29,13 @@ public class BombBehavior : MonoBehaviour
     {
         //Debug.Log("SPEEEEEEEEEED   =" + speed);
         if (Time.time - starttime > 2.5f) { 
-            alpha += 20;
+            alpha += 0.02f;
+            
+            
             gameObject.transform.localScale += new Vector3(.01f, .01f, .01f);
-          //  gameObject.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, alpha);
+            Color color = gameObject.GetComponent<SpriteRenderer>().color;
+            Debug.Log(color);
+            gameObject.GetComponent<SpriteRenderer>().color =  new Color(color.r, color.g, color.b, alpha);
         }
     }
 
