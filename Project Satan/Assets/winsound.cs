697499a3ec5ebec5f02880783c4501fbbaaadeef
mat13109 <letesteurdelepique@gmail.com>
2019-10-25 15:41:21 +0200

    WINENR

---
+++ Project Satan/Assets/winsound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class winsound : MonoBehaviour
+{
+    [SerializeField] AudioClip audioClip;
+    [SerializeField] AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(yes());
+    }
+
+    // Update is called once per frame
+    IEnumerator yes()
+    {
+        yield return new WaitForSecondsRealtime(3);
+        audioSource.clip = audioClip;
+        audioSource.Stop();
+        audioSource.Play();
+    }
+}
